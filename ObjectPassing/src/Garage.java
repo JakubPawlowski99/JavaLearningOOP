3f995a4f6688002671579867616e6922b91bd6d7
JakubPawlowski99 <56391766+JakubPawlowski99@users.noreply.github.com>
2023-12-07 16:02:31 +0100

    Add files via upload

---
+++ ObjectPassing/src/Garage.java
@@ -0,0 +1,12 @@
+
+public class Garage {
+
+	void park(Car car) {
+		System.out.println("The " + car.name + " is parked in the garage");
+	}
+	
+	void drive(Car car) {
+		System.out.println("The " + car.name + " is driving");
+	}
+		
+}
---
+++ ObjectPassing/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Garage garage = new Garage();
+		
+		Car car1 = new Car("BMW");
+
+		Car car2 = new Car("Tesla");
+		garage.park(car2);
+		garage.drive(car1);
+	}
+
+}
