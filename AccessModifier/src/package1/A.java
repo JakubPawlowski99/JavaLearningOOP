11d04acccbe4f5ceabbb833f321e75cc3db16e04
JakubPawlowski99 <56391766+JakubPawlowski99@users.noreply.github.com>
2023-12-11 15:36:40 +0100

    Add files via upload

---
+++ AccessModifier/src/package1/A.java
@@ -0,0 +1,15 @@
+package package1;
+import package2.*;
+
+public class A {
+//	protected String protectedMessage = "This is protected";
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//C c = new C();
+		//System.out.println(c.publicMessage);
+		B b = new B();
+		//Asub asub = new Asub();
+		//System.out.println(b.privateMessage);
+	}
+}
