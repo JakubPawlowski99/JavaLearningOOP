38bf5e899e9b7194d7c591dd57055a73fcdabe8d
JakubPawlowski99 <56391766+JakubPawlowski99@users.noreply.github.com>
2024-03-15 12:16:55 +0100

    Add files via upload

---
+++ FileLoad/src/MyFrame.java
@@ -0,0 +1,40 @@
+import java.awt.FlowLayout;
+import java.awt.event.*;
+import java.io.File;
+
+import javax.swing.*;
+
+public class MyFrame extends JFrame implements ActionListener{
+	JButton button;
+	MyFrame(){
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLayout(new FlowLayout());
+		
+		button = new JButton("Select File");
+		button.addActionListener(this);
+		this.add(button);
+		this.pack();
+		this.setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource()==button) {
+			
+			JFileChooser fileChooser = new JFileChooser();
+			
+			fileChooser.setCurrentDirectory(new File("."));
+			
+			int response = fileChooser.showOpenDialog(null); //select file to open returns int value based on selected option
+			
+			
+			//if file is selected
+			if(response == JFileChooser.APPROVE_OPTION) {
+				File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+				System.out.println(file);
+			}
+			
+		}
+	}
+}
